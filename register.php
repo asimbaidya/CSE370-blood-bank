70a3f044c9842828ef976aa6ac85295d73652ef1
penguin <asimbaidya.me@gmail.com>
2022-12-16 01:30:46 +0600

    favion

--- register.php
+++ register.php
@@ -11,6 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <!-- custom css -->
     <link rel="stylesheet" href="/static/styles/style.css">
+    <link href="/project/asset/favicon.png" rel="icon" type="image/png" />
     <style>
         .navbar {
             font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
