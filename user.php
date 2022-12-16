1843ad31a4782494da561d182b64e9219cfa4896
penguin <asimbaidya.me@gmail.com>
2022-12-16 20:27:39 +0600

    more more shits

--- user.php
+++ user.php
@@ -73,18 +73,57 @@ include_once('../db/config.php');
     </nav>
 
     <!----------------------------- form ------------------------------  -->
-    <div class="container">
-        <?php
-        if ($_SESSION['user_loggedin']) {
-            echo "<h1>User :" . $_SESSION['useremail'];
-        } else {
-            echo "Please  login to visit you profile!";
-            echo '<a href="/project/login.php">
-            <span class="badge text-bg-info">Info</span>
-        </a>';
-        }
-        ?>
-    </div>
+    <?php
+    // query & variable unpack
+    $name = $_SESSION['useremail'];
+    $address = "TODO";
+    ?>
+    <section class="h-100 gradient-custom-2">
+        <div class="container h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col col-lg-9 col-xl-7">
+                    <div class="card">
+                        <div class="rounded-top text-white d-flex flex-row" style="background-color: #000; height:200px;">
+                            <div class="ms-4 mt-5 d-flex flex-column" style="width: 150px;">
+                                <img src="/project/asset/user-avater-male.png" alt="Generic placeholder image" class="img-fluid img-thumbnail mt-4 mb-2" style="width: 150px; z-index: 1">
+                            </div>
+                            <div class="ms-3" style="margin-top: 130px;">
+                                <h5>email: <?php echo $name ?></h5>
+                                <span><?php echo $address ?></span>
+                            </div>
+                        </div>
+                        <div class="p-4 text-black" style="background-color: #f8f9fa;">
+                            <div class="d-flex justify-content-end text-center ">
+                                <div>
+                                    <p class="mb-1 h5">253</p>
+                                    <p class="small text-muted mb-0">Photos</p>
+                                </div>
+                                <div class="px-3">
+                                    <p class="mb-1 h5">1026</p>
+                                    <p class="small text-muted mb-0">Followers</p>
+                                </div>
+                                <div>
+                                    <p class="mb-1 h5">478</p>
+                                    <p class="small text-muted mb-0">Following</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body p-4 text-black">
+                            <div class="mb-5">
+                                <p class="lead fw-normal mb-1">About</p>
+                                <div class="p-4" style="background-color: #f8f9fa;">
+                                    <p class="font-italic mb-1">Web Developer</p>
+                                    <p class="font-italic mb-1">Lives in New York</p>
+                                    <p class="font-italic mb-0">Photographer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
 
 </body>
 
