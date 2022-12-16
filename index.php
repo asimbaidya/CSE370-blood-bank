aed923cb1b630a8a9a36069f34c09b5a7560902a
penguin <asimbaidya.me@gmail.com>
2022-12-17 02:28:34 +0600

    footer applied to index.html

--- index.php
+++ index.php
@@ -161,6 +161,62 @@ require_once('./db/config.php');
             </button>
         </div>
     </div>
+    <!----------------------------- Footers -----------------------------  -->
+    <div class="b-example-divider"></div>
+    <div class="container">
+        <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+            <div class="col mb-3">
+                <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32">
+                        <use xlink:href="#bootstrap" />
+                    </svg>
+                </a>
+                <p class="text-muted">&copy;2022</p>
+            </div>
+
+            <div class="col mb-3"></div>
+
+            <div class="col mb-3">
+                <h5>Section</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2">
+                        <a href="#" class="nav-link p-0 text-muted">Home</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a href="#" class="nav-link p-0 text-muted">Features</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col mb-3">
+                <h5>Section</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2">
+                        <a href="#" class="nav-link p-0 text-muted">Home</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a href="#" class="nav-link p-0 text-muted">Features</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col mb-3">
+                <h5>Section</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2">
+                        <a href="#" class="nav-link p-0 text-muted">Home</a>
+                    <li class="nav-item mb-2">
+                        <a href="#" class="nav-link p-0 text-muted">FAQs</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a href="#" class="nav-link p-0 text-muted">About</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+
+    <!---------------------------- javascript ---------------------------  -->
     <script>
         const myCarouselElement = document.querySelector('#carousel_slides')
         const carousel = new bootstrap.Carousel(myCarouselElement, {
